0402ec22851b54b8286b29a9a0911b909eb833ed
Yüksel Toprak <yuekseltoprak@gmail.com>
2021-10-07 12:49:46 +0300

    Initial Commit

---
+++ Learning-SwiftUI/Learning_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUIApp.swift
+//  Learning-SwiftUI
+//
+//  Created by Yüksel Toprak on 07.10.21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
